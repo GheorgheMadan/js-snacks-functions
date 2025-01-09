5832e7504f1a4f37bba8512c93702d0b5efe95f3
Gheorghe <madangheorghe.30@gmail.com>
2025-01-09 15:29:39 +0100

    fine secondo esercizio

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,15 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
-
 const userName = 'Mario';
-console.log(saluto(userName))
 
 // Dichiara la funzione qui.
 function saluto(nome){
     return "Ciao " + nome
 }
 
+// versione con arrow function 
+const saluto2 = nome => "Ciao " + nome
+
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluto(userName))
+console.log(saluto2(userName))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,36 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function ArrayIniziali(arr){
+    // creo un array vuoto 
+    const iniziali = [];
+
+    // creo un ciclo che sottrae l'iniziale di ogni stringa dell'array
+    for (let i = 0; i < arr.length; i++){
+        iniziali.push(arr[i][0]);
+    }
+    return iniziali;
+        
+}
+// versione arrow function
+const ArrayIniziali2 = arr =>{
+    // creo un array vuoto 
+    const iniziali2 = [];
+
+    // creo un ciclo che sottrae l'iniziale di ogni stringa dell'array
+    for (let i = 0; i < arr.length; i++){
+        iniziali2.push(arr[i][0]);
+    }
+    return iniziali2;
+        
+}
+// invoco l'arrow function
+const inizialiNomi2 = ArrayIniziali2(names)
+console.log(inizialiNomi2)
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const inizialiNomi = ArrayIniziali(names)
+console.log(inizialiNomi)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
